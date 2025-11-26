d9b140ae9cc82a24e54b651a57f85d0b7e30e655
Adi Siregar <89087694+NaufalAdiPS@users.noreply.github.com>
2025-11-26 14:53:07 +0700

    Update The_Report.sql

--- Soal 1 : The Report/The_Report.sql
+++ Soal 1 : The Report/The_Report.sql
@@ -1,1 +1,29 @@
+/*
+Enter your query here.
+*/
+
+WITH mapped AS (
+    SELECT
+        s.id,
+        s.name,
+        s.marks,
+        g.grade
+    FROM Students s
+    JOIN Grades g
+        ON s.marks BETWEEN g.min_mark AND g.max_mark
+)
+SELECT
+    CASE
+        WHEN grade < 8 THEN 'NULL'
+        ELSE name
+    END AS Name,
+    grade AS Grade,
+    marks AS Mark
+FROM mapped
+ORDER BY
+    grade DESC,
+    CASE WHEN grade >= 8 THEN name ELSE NULL END ASC,
+    CASE WHEN grade < 8 THEN marks ELSE NULL END ASC;
+
+
 
