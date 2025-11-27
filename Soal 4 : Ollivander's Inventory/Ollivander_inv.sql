0dc5f4e44dd1ad19c15ef326072a31a376c5568d
Adi Siregar <89087694+NaufalAdiPS@users.noreply.github.com>
2025-11-27 14:30:02 +0700

    Update Ollivander_inv.sql

--- Soal 4 : Ollivander's Inventory/Ollivander_inv.sql
+++ Soal 4 : Ollivander's Inventory/Ollivander_inv.sql
@@ -1,1 +1,33 @@
+SET NOCOUNT ON;
 
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+WITH data_wand AS (
+    SELECT
+        w.id,
+        wp.age,
+        w.power,
+        w.coins_needed,
+        min(w.coins_needed) OVER (PARTITION BY wp.age, w.power) AS coins_min
+    FROM Wands w 
+    JOIN Wands_Property wp 
+        ON w.code = wp.code 
+    WHERE wp.is_evil = 0
+)
+
+
+SELECT
+    id,
+    age,
+    coins_needed,
+    power
+FROM data_wand
+WHERE coins_needed = coins_min
+ORDER BY
+    power DESC,
+    age DESC;
+    
+go
